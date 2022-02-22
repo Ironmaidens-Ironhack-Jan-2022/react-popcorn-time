5f3dd4165f15f92a101546cb286d0c8aecc714b4
Joanne Seiler <joanne@posteo.de>
2022-02-22 15:57:39 +0100

    add conditional rendering

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,11 +2,19 @@ import "./Movie.css"
 
 export default function Movie(props) {
 
+  let imgTag;
+  if(props.imgURL){
+    imgTag = <img src={props.imgURL} alt={props.title} />;
+  } else {
+    imgTag = <img src="https://via.placeholder.com/182x268" alt="" />;
+  }
 
   return (
     <div className="Movie">
         <h2>{props.title}</h2>
-        <img src={props.imgURL} alt={props.title} />
+        
+        {imgTag}
+
         <p>
             Year: {props.year} <br />
             Rating: {props.rating} <br />
