616e4db372838cc29053935c8b673b7e37d86ebf
Joanne Seiler <joanne@posteo.de>
2022-02-22 16:04:08 +0100

    conditional rendering using ternary operator

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,18 +2,24 @@ import "./Movie.css"
 
 export default function Movie(props) {
 
-  let imgTag;
-  if(props.imgURL){
-    imgTag = <img src={props.imgURL} alt={props.title} />;
-  } else {
-    imgTag = <img src="https://via.placeholder.com/182x268" alt="" />;
-  }
+//---- CONDITIONAL RENDERING WITH IF CONDITION ---
+  // let imgTag;
+  // if(props.imgURL){
+  //   imgTag = <img src={props.imgURL} alt={props.title} />;
+  // } else {
+  //   imgTag = <img src="https://via.placeholder.com/182x268" alt="" />;
+  // }
 
   return (
     <div className="Movie">
         <h2>{props.title}</h2>
-        
-        {imgTag}
+
+        {/* ---- CONDITIONAL RENDERING WITH TERNARY OPERATOR --- */}
+        {
+          props.imgURL 
+          ? <img src={props.imgURL} alt={props.title} />
+          : <img src="https://via.placeholder.com/182x268" alt="" />
+        }
 
         <p>
             Year: {props.year} <br />
