8e0577d5922e3c7a68842a42f95eccad64e44db3
Joanne Seiler <joanne@posteo.de>
2022-02-23 16:23:43 +0100

    create AddMovie to move functionality to add movie in a separate component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { useState } from "react";
 import moviesArr from "./data/movies.json";
+
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import Controls from "./components/Controls";
+import AddMovie from "./components/AddMovie";
 
 function App() {
 
   const [movies, setMovies] = useState(moviesArr);
 
-  const [titleInput, setTitleInput] = useState("");
-  const [yearInput, setYearInput] = useState(2000);
-  const [ratingInput, setRatingInput] = useState("");
-
+  
   const filterTopRated = (minRating = 9) => {
       const newList = moviesArr.filter( (element) => {
         return element.rating >= minRating;
@@ -21,63 +20,17 @@ function App() {
       setMovies(newList);
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
 
-    const newMovie = {
-      title: titleInput,
-      year: yearInput,
-      rating: ratingInput
-    }
-
-    setMovies( [newMovie, ...movies] ); // update list of movies
-    setTitleInput("") // clear form
+  const createMovie = (movieDetails) => {
+    setMovies( [movieDetails, ...movies] ); // update list of movies
   }
-
-  const handleTitleInput = (e) => {
-    setTitleInput(e.target.value);
-  }
-
-  const handleYearInput = (e) => {
-    setYearInput(e.target.value);
-  }
-
-  const handleRatingInput = (e) => {
-    setRatingInput(e.target.value);
-  } 
 
 
   return (
     <div className="App">
       <Header numberOfMovies={movies.length} />
       
-
-
-      <form onSubmit={handleSubmit}>
-
-        <label>Title:
-          <input type="text" name="title" value={titleInput} onChange={handleTitleInput} />
-        </label>
-
-        <label>Year:
-          <input type="number" name="year" value={yearInput} onChange={handleYearInput} />
-        </label>
-
-        <label>Rating:
-          <input 
-            type="number" 
-            min={1} 
-            max={10} 
-            name="rating" 
-            value={ratingInput} 
-            onChange={handleRatingInput} 
-          />
-        </label>
-
-        <button type="submit">Add a Movie</button>
-      </form>
-
-
+      <AddMovie createMovie={createMovie} />
 
       <Controls displayTop={filterTopRated} />
       <Main moviesArr={movies} />
@@ -87,4 +40,3 @@ function App() {
 }
 
 export default App;
-
--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,13 +1,76 @@
+import { useState } from "react";
 import "./AddMovie.css"
 
 function AddMovie(props) {
-    return (
-        <div className="AddMovie">
-            <h4>Add a Movie</h4>
 
-            
-        </div>
-    );
+  const [titleInput, setTitleInput] = useState("");
+  const [yearInput, setYearInput] = useState(2000);
+  const [ratingInput, setRatingInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newMovie = {
+      title: titleInput,
+      year: yearInput,
+      rating: ratingInput
+    }
+
+    props.createMovie(newMovie);
+
+    clearForm();
+  }
+
+  const clearForm = () => {
+    setTitleInput("");
+    setYearInput(2000);
+    setRatingInput("");
+  }
+
+
+  // Extra: improve code, handling all in one single method 
+  // https://stackoverflow.com/a/67234242/11298742
+
+  const handleTitleInput = (e) => {
+    setTitleInput(e.target.value);
+  }
+
+  const handleYearInput = (e) => {
+    setYearInput(e.target.value);
+  }
+
+  const handleRatingInput = (e) => {
+    setRatingInput(e.target.value);
+  } 
+
+
+  return (
+    <div className="AddMovie">
+      <form onSubmit={handleSubmit}>
+
+        <label>Title:
+          <input type="text" name="title" value={titleInput} onChange={handleTitleInput} />
+        </label>
+
+        <label>Year:
+          <input type="number" name="year" value={yearInput} onChange={handleYearInput} />
+        </label>
+
+        <label>Rating:
+          <input
+            type="number"
+            min={1}
+            max={10}
+            name="rating"
+            value={ratingInput}
+            onChange={handleRatingInput}
+          />
+        </label>
+
+        <button type="submit">Add a Movie</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddMovie;
