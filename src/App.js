038c7d2861e4b6b11cc6e64f68dd64a51ca748b8
Joanne Seiler <joanne@posteo.de>
2022-02-23 14:54:05 +0100

    create form and functionality to add a new movie

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import moviesArr from "./data/movies.json";
+
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import Controls from "./components/Controls";
+import AddMovie from "./components/AddMovie";
 
 function App() {
 
   const [movies, setMovies] = useState(moviesArr);
+
+  const [titleInput, setTitleInput] = useState("");
 
   const filterTopRated = (minRating = 9) => {
       const newList = moviesArr.filter( (element) => {
@@ -17,10 +21,38 @@ function App() {
       setMovies(newList);
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newMovie = {
+      title: titleInput
+    }
+
+    setMovies( [newMovie, ...movies] );
+  }
+
+  const handleTitleInput = (e) => {
+    setTitleInput(e.target.value);
+  }
 
   return (
     <div className="App">
       <Header numberOfMovies={movies.length} />
+      
+      
+      {/* <AddMovie /> */}
+
+      <form onSubmit={handleSubmit}>
+
+        <label>Title:
+          <input type="text" name="title" value={titleInput} onChange={handleTitleInput} />
+        </label>
+
+        <button type="submit">Add a Movie</button>
+      </form>
+
+
+
       <Controls displayTop={filterTopRated} />
       <Main moviesArr={movies} />
       <Footer />
@@ -29,3 +61,5 @@ function App() {
 }
 
 export default App;
+
+
---
+++ src/components/AddMovie.js
@@ -0,0 +1,13 @@
+import "./AddMovie.css"
+
+function AddMovie(props) {
+    return (
+        <div className="AddMovie">
+            <h4>Add a Movie</h4>
+
+            
+        </div>
+    );
+}
+
+export default AddMovie;
