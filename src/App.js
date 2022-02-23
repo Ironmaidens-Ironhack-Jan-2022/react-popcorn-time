277159c48b558776b4a5ecd7133e114b06031598
Joanne Seiler <joanne@posteo.de>
2022-02-23 15:34:47 +0100

    functionality to create a movie

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import moviesArr from "./data/movies.json";
-
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import Controls from "./components/Controls";
-import AddMovie from "./components/AddMovie";
 
 function App() {
 
   const [movies, setMovies] = useState(moviesArr);
 
   const [titleInput, setTitleInput] = useState("");
+  const [yearInput, setYearInput] = useState(2000);
+  const [ratingInput, setRatingInput] = useState("");
 
   const filterTopRated = (minRating = 9) => {
       const newList = moviesArr.filter( (element) => {
@@ -25,27 +25,53 @@ function App() {
     e.preventDefault();
 
     const newMovie = {
-      title: titleInput
+      title: titleInput,
+      year: yearInput,
+      rating: ratingInput
     }
 
-    setMovies( [newMovie, ...movies] );
+    setMovies( [newMovie, ...movies] ); // update list of movies
+    setTitleInput("") // clear form
   }
 
   const handleTitleInput = (e) => {
     setTitleInput(e.target.value);
   }
 
+  const handleYearInput = (e) => {
+    setYearInput(e.target.value);
+  }
+
+  const handleRatingInput = (e) => {
+    setRatingInput(e.target.value);
+  } 
+
+
   return (
     <div className="App">
       <Header numberOfMovies={movies.length} />
       
-      
-      {/* <AddMovie /> */}
+
 
       <form onSubmit={handleSubmit}>
 
         <label>Title:
           <input type="text" name="title" value={titleInput} onChange={handleTitleInput} />
+        </label>
+
+        <label>Year:
+          <input type="number" name="year" value={yearInput} onChange={handleYearInput} />
+        </label>
+
+        <label>Rating:
+          <input 
+            type="number" 
+            min={1} 
+            max={10} 
+            name="rating" 
+            value={ratingInput} 
+            onChange={handleRatingInput} 
+          />
         </label>
 
         <button type="submit">Add a Movie</button>
@@ -61,5 +87,4 @@ function App() {
 }
 
 export default App;
-
 
